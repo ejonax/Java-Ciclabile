140893e3c0c1ef22367e4ec976fc87cca3970adf
ejonax <ejonax@yahoo.it>
2025-02-22 17:11:39 +0100

    first commit

---
+++ src/package1/CicloArray.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class CicloArray {
+
+   
+    private int[] array;
+    private int index;
+
+    public CicloArray(int[] array){
+        this.array=array;
+    }
+
+    public boolean hasAncoraElementi() {
+        return index<array.length;
+    }
+
+    public int getElementoSuccessivo(){
+        if (!hasAncoraElementi()){
+            System.out.println("Siamo alla fine dell'array, non ci sono elementi da ritornare");
+           
+        }
+        int i=index;// se stampo array[index] allora si stampera index ++ che abbiamo incrementato una incrementazione prima
+        index++;
+        return array[i];
+    }
+}
---
+++ src/package1/usaCicloArray.java
@@ -0,0 +1,39 @@
+package package1;
+
+public class usaCicloArray {
+    public static void main(String[] args) {
+        
+        int[] ciclo = {1,2,3,4,5};
+
+        CicloArray cicloArray=new CicloArray(ciclo);
+        int i = 0;
+
+         //cicloArray.getElementoSuccessivo(); 
+        System.out.println(cicloArray.hasAncoraElementi());//T or F
+        System.out.println(cicloArray.getElementoSuccessivo());// il valore stesso dell'elemento
+
+        //cicloArray.getElementoSuccessivo(); 
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+
+       // cicloArray.getElementoSuccessivo();
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+
+        //cicloArray.getElementoSuccessivo();
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+
+        //cicloArray.getElementoSuccessivo();
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+
+        //cicloArray.getElementoSuccessivo();
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+
+        //cicloArray.getElementoSuccessivo();
+        System.out.println(cicloArray.hasAncoraElementi());
+        System.out.println(cicloArray.getElementoSuccessivo());
+    }
+}
